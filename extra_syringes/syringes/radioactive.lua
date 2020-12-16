bb66e4c9f30896618f917233f1f2bba3fa524a2c
akitary <akitary.commissions@gmail.com>
2020-12-16 21:47:29 +0100

    syringes update

--- extra_syringes/syringes/radioactive.lua
+++ extra_syringes/syringes/radioactive.lua
@@ -8,42 +8,45 @@ local RadioActive = {
     DAMAGE_MULTIPLIER = 2,
     DAMAGE_FRAME = 0.5 * 30,
     BLACKHEART_CHANCE = 0.05,
-    SERPENTS_KISS_MULTIPLIER = 1.5,
+    SERPENTS_KISS_MULTIPLIER = 1.5
 }
 
 RadiactiveHaloEntity = nil
 RadiactiveHaloCounter = 0
-
-local function BurnEnemy(player, entity)
-    local damage = player.Damage * RadioActive.DAMAGE_MULTIPLIER * RadiactiveHaloCounter
-    entity:AddBurn(EntityRef(player), RadioActive.DAMAGE_FRAME, damage)
-end
 
 local function GetSerpentsKissMultiplier(player)
     return player:HasCollectible(CollectibleType.COLLECTIBLE_SERPENTS_KISS) and RadioActive.SERPENTS_KISS_MULTIPLIER or 1
 end
 
 local function GetLuckMultiplier(player)
-    if player.Luck <= 0 then return 1 end
-    return math.min(player.Luck, 10) / 10 + 1
-end
-
-local function AddBlackHeartDrop(player, entity)
-    blackHeartChance = RadioActive.BLACKHEART_CHANCE * GetSerpentsKissMultiplier(player) * GetLuckMultiplier(player)
-    if(rng:RandomFloat() < blackHeartChance) then
-        entity:AddEntityFlags(EntityFlag.FLAG_SPAWN_BLACK_HP) 
+    if player.Luck <= 0 then
+        return 1
     end
+    return math.min(player.Luck, 10) / 10 + 1
 end
 
 local function ApplyRadioactiveEffects(player, targets)
 
     for key, entity in pairs(targets) do
         if entity:IsActiveEnemy() and entity.FrameCount > 0 then
-            BurnEnemy(player, entity)
-            AddBlackHeartDrop(player, entity)
-        end 
-    end
 
+            local damage = player.Damage * RadioActive.DAMAGE_MULTIPLIER * RadiactiveHaloCounter
+            entity:AddBurn(EntityRef(player), RadioActive.DAMAGE_FRAME, damage)
+
+            blackHeartChance = RadioActive.BLACKHEART_CHANCE * GetSerpentsKissMultiplier(player) * GetLuckMultiplier(player)
+            if (rng:RandomFloat() < blackHeartChance) then
+                entity:AddEntityFlags(EntityFlag.FLAG_SPAWN_BLACK_HP)
+            end
+        end
+    end
+end
+
+-- TODO: custom fire entity
+local function SpawnToxicFire(player, entity)
+    local entityPos = entity.Position
+    if player.Position:Distance(entityPos) < RadioActive.RADIUS then
+        fire = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.HOT_BOMB_FIRE, 0, entityPos, Vector(0, 0), player):ToEffect();
+    end
 end
 
 local function RemoveRadioActiveHalo()
@@ -52,14 +55,15 @@ local function RemoveRadioActiveHalo()
 end
 
 local function SpawnRadioActiveHalo(player)
-    if(RadiactiveHaloEntity == nil) then
+    if (RadiactiveHaloEntity == nil) then
         RadiactiveHaloEntity = Isaac.Spawn(EntityType.ENTITY_EFFECT, RadioActiveHalo, 0, player.Position, Vector(0, 0), player):ToEffect();
         RadiactiveHaloEntity:FollowParent(player)
         RadiactiveHaloEntity:AddEntityFlags(EntityFlag.FLAG_PERSISTENT)
     end
 end
 
-M_SYR.EFF_TAB[mod.Syringes.Radioactive] = {
+M_SYR.EFF_TAB[mod.Syringes.Radioactive] = 
+{
     ID = mod.Syringes.Radioactive,
     Type = M_SYR.TYPES.Negative,
     Name = "Radioactive",
@@ -68,24 +72,38 @@ M_SYR.EFF_TAB[mod.Syringes.Radioactive] = {
     Duration = 450,
     Counterpart = M_SYR.TOT_SYR.DPSDampener,
     Weight = 1,
-    
+
     Effect = 
     {
         [1] = 
         {
-             Function = function(_)
-                local player = M_SYR.GetPlayerUsingActive() 
-
+            Function = function(_)
+                local player = M_SYR.GetPlayerUsingActive()
                 if mod:IsCorrectSyringe(player, mod.Syringes.Radioactive) and mod:IsOnFrame(RadioActive.DAMAGE_FRAME) then
                     local targets = Isaac.FindInRadius(player.Position, RadioActive.RADIUS, EntityPartition.ENEMY)
                     ApplyRadioactiveEffects(player, targets)
                 end
             end,
-            
-            Callback = ModCallbacks.MC_POST_UPDATE 
-       }
+
+            Callback = ModCallbacks.MC_POST_UPDATE
+        },
+        [2] = 
+        {
+            Function = function(_, entity)
+                local player = M_SYR.GetPlayerUsingActive()
+                SpawnToxicFire(player, entity)
+            end,
+            Callback = ModCallbacks.MC_POST_NPC_DEATH
+        },
+        [3] = 
+        {
+            Function = function(_)
+
+            end,
+            Callback = ModCallbacks.MC_POST_NPC_DEATH
+        }
     },
-    
+
     OnUse = function(idx)
 
         local player = M_SYR.GetPlayerUsingActive()
@@ -93,9 +111,9 @@ M_SYR.EFF_TAB[mod.Syringes.Radioactive] = {
         SpawnRadioActiveHalo(player)
         RadiactiveHaloCounter = RadiactiveHaloCounter + 1
     end,
-    
+
     Post = function(player)
         RadiactiveHaloCounter = RadiactiveHaloCounter - 1
         RemoveRadioActiveHalo()
-    end,
+    end
 }
--- extra_syringes/util.lua
+++ extra_syringes/util.lua
@@ -1,36 +1,27 @@
 local game = Game()
 local sfx = SFXManager()
+local mod = extraSyringes
 
-function extraSyringes:IsCorrectSyringe(entity, id, checkIfPlayer)
-    if entity == nil then 
-        return false 
-    end
-
-    if checkIfPlayer ~= false then
-        if entity.Type ~= EntityType.ENTITY_PLAYER then 
-            return false
-        end
-    end
-
-    syringeID = M_SYR.EFF_TAB[id].ID
-    return entity:GetData().SyringeEffects[syringeID] ~= nil
+function mod:IsCorrectSyringe(player, syringe)
+    syringeID = M_SYR.EFF_TAB[syringe].ID
+    return M_SYR.CheckForEffect(player, syringeID)
 end
 
-function extraSyringes:PositiveFeedback(player)
+function mod:PositiveFeedback(player)
     sfx:Play(SoundEffect.SOUND_POWERUP_SPEWER, 1, 0, false, 1)
     player:AnimateHappy()
 end
 
-function extraSyringes:NegativeFeedback(player)
+function mod:NegativeFeedback(player)
     sfx:Play(SoundEffect.SOUND_THUMBS_DOWN, 1, 0, false, 1)
     player:AnimateSad()
 end
 
-function extraSyringes:UpdateCache(player, cacheFlags)
+function mod:UpdateCache(player, cacheFlags)
     player:AddCacheFlags(cacheFlags)
     player:EvaluateItems()
 end
 
-function extraSyringes:IsOnFrame(frame)
+function mod:IsOnFrame(frame)
     return game:GetFrameCount() % frame == 0
 end
